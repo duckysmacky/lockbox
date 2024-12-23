d65e02c924472dfeb24c58c559138b5647697daf
DuckySmacky <kriperoka@gmail.com>
2024-12-23 19:07:55 +0300

    Fix CLI test running for Linux and MacOS

--- tests/common/commands.rs
+++ tests/common/commands.rs
@@ -6,8 +6,12 @@ use std::process::{Command, Output};
 use std::path::PathBuf;
 use super::PASSWORD;
 
+fn base_command() -> Command {
+    Command::new("target/debug/lockbox.exe")
+}
+
 pub fn run_box(path: &PathBuf, extra_args: &[&str]) -> Output {
-    Command::new("lockbox")
+    base_command()
         .args(&["-v", "-p", PASSWORD, "box", path.to_str().unwrap()])
         .args(extra_args)
         .output()
@@ -15,7 +19,7 @@ pub fn run_box(path: &PathBuf, extra_args: &[&str]) -> Output {
 }
 
 pub fn run_unbox(path: &PathBuf, extra_args: &[&str]) -> Output {
-    Command::new("lockbox")
+    base_command()
         .args(&["-v", "-p", PASSWORD, "unbox", path.to_str().unwrap()])
         .args(extra_args)
         .output()
@@ -23,56 +27,56 @@ pub fn run_unbox(path: &PathBuf, extra_args: &[&str]) -> Output {
 }
 
 pub fn run_profile_new(name: &str) -> Output {
-    Command::new("lockbox")
+    base_command()
         .args(&["-v", "-p", PASSWORD, "profile", "new", name])
         .output()
         .expect("Failed to execute the \"profile create\" command")
 }
 
 pub fn run_profile_delete(name: &str) -> Output {
-    Command::new("lockbox")
+    base_command()
         .args(&["-v", "-p", PASSWORD, "profile", "delete", name])
         .output()
         .expect("Failed to execute the \"profile delete\" command")
 }
 
 pub fn run_profile_select(name: &str) -> Output {
-    Command::new("lockbox")
+    base_command()
         .args(&["-v", "-p", PASSWORD, "profile", "select", name])
         .output()
         .expect("Failed to execute the \"profile select\" command")
 }
 
 pub fn run_profile_get() -> Output {
-    Command::new("lockbox")
+    base_command()
         .args(&["-v", "-p", PASSWORD, "profile", "get"])
         .output()
         .expect("Failed to execute the \"profile get\" command")
 }
 
 pub fn run_profile_list() -> Output {
-    Command::new("lockbox")
+    base_command()
         .args(&["-v", "-p", PASSWORD, "profile", "list"])
         .output()
         .expect("Failed to execute the \"profile list\" command")
 }
 
 pub fn run_key_new() -> Output {
-    Command::new("lockbox")
+    base_command()
         .args(&["-v", "-p", PASSWORD, "key", "new"])
         .output()
         .expect("Failed to execute the \"key new\" command")
 }
 
 pub fn run_key_get() -> Output {
-    Command::new("lockbox")
+    base_command()
         .args(&["-v", "-p", PASSWORD, "key", "get"])
         .output()
         .expect("Failed to execute the \"key get\" command")
 }
 
 pub fn run_key_set(key: &str) -> Output {
-    Command::new("lockbox")
+    base_command()
         .args(&["-v", "-p", PASSWORD, "key", "set", key])
         .output()
         .expect("Failed to execute the \"key set\" command")
