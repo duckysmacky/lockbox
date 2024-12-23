faa3a706244f1d74a1ff5cd5b777e0bbe9ca9300
DuckySmacky <kriperoka@gmail.com>
2024-12-23 14:39:06 +0300

    Improve error handling for tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,38 +1,49 @@
+//! Contains common functions and constatants for running tests.
+
 pub mod commands;
 
-use std::{env, fs};
+use std::{env, fs, io};
 use std::path::{Path, PathBuf};
 
 pub const PASSWORD: &str = "test1234"; // just a password for testing
 pub const ORIGINAL_DIR: &str = "files/original";
 pub const TEST_DIR: &str = "files/test";
 
+/// Global test environment setup (must be run before each test)
 pub fn setup() {
-    copy_original()
+    if let Err(err) = copy_original() {
+        panic!("Unable to copy original test files: {}", err)
+    }
 }
 
+/// Global test environment cleanup (must be run after each test)
 pub fn cleanup() {
-    copy_original()
+    if let Err(err) = copy_original() {
+        panic!("Unable to copy original test files: {}", err)
+    }
 }
 
-fn copy_original() {
+/// Copies original test files for use in tests
+fn copy_original() -> io::Result<()> {
     let test_dir = Path::new(TEST_DIR);
 
     if test_dir.exists() {
-        fs::remove_dir_all(test_dir).unwrap();
+        fs::remove_dir_all(test_dir)?;
     }
-    fs::create_dir(test_dir).unwrap();
+    fs::create_dir(test_dir)?;
 
-    for entry in fs::read_dir(ORIGINAL_DIR).unwrap() {
-        let original_file = entry.unwrap().path();
+    for entry in fs::read_dir(ORIGINAL_DIR)? {
+        let original_file = entry?.path();
 
         if original_file.is_file() {
             let file_name = original_file.file_name().unwrap();
             let test_file = test_dir.join(file_name);
 
-            fs::copy(&original_file, &test_file).unwrap();
+            fs::copy(&original_file, &test_file)?;
         }
     }
+
+    Ok(())
 }
 
 fn _get_data_dir() -> PathBuf {
